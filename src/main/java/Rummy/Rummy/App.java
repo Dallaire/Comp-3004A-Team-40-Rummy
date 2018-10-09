723f9327720f57d2f912e39dd95f991d22bb0c24
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2018-10-09 02:44:29 -0400

    app: modifications to the app class

--- src/main/java/Rummy/Rummy/App.java
+++ src/main/java/Rummy/Rummy/App.java
@@ -1,13 +1,33 @@
 package Rummy.Rummy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Table table;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	gameSetup();
+    	
+    	
     }
+    
+    public static void gameSetup() {
+    	Scanner scanner=new Scanner(System.in);
+    	
+    	System.out.println("how many players are playing?");
+        int input=scanner.nextInt();
+         System.out.println(input);
+    	 table= new Table();
+    	        
+	}
+    public static void shareCards() {
+		for(int i=0;i<14;i++) {
+			table.getPlayer(i).addTile(table.getTile());
+		}
+	}
 }
