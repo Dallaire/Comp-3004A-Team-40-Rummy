4d565bed33909486db0a77344ff146d4d56493a9
Poerilla <tshepok13@gmail.com>
2018-10-22 12:37:48 -0400

    last before branch

--- src/test/java/Rummy/Rummy/TableTest2.java
+++ src/test/java/Rummy/Rummy/TableTest2.java
@@ -3,5 +3,10 @@ package Rummy.Rummy;
 import junit.framework.TestCase;
 
 public class TableTest2 extends TestCase {
-
+	
+    public void testPlayerMeld() {
+    	Game input = new Game();
+    	assertEquals("M", input.getPlayerInput());
+    }
+	
 }
