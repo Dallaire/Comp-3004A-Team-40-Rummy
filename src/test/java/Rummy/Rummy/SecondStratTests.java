752d158b80d30105942b3d90dc482f0a74dd90e6
Dallaire <jacobdallaire@yahoo.ca>
2018-11-02 13:54:52 -0400

    Test playing all tiles with adding to existing melds

--- src/test/java/Rummy/Rummy/SecondStratTests.java
+++ src/test/java/Rummy/Rummy/SecondStratTests.java
@@ -60,4 +60,33 @@ public class SecondStratTests extends TestCase {
 		
 		assertTrue(player.getHand().isEmpty());
 	}
+	
+	public void testPlayAllTilesWithTable() {
+		Table.loadPlayers();
+		Table.getMelds().clear();
+		SecondStrategy player = (SecondStrategy)Table.getPlayer(2);
+		player.addTile(new Tile(Color.B, 1));
+		player.addTile(new Tile(Color.B, 2));
+		player.addTile(new Tile(Color.B, 3));
+		player.addTile(new Tile(Color.B, 4));
+		player.addTile(new Tile(Color.B, 5));
+		player.addTile(new Tile(Color.B, 8));
+		player.addTile(new Tile(Color.O, 1));
+		player.addTile(new Tile(Color.O, 2));
+		player.addTile(new Tile(Color.O, 3));
+		player.addTile(new Tile(Color.O, 4));
+		player.addTile(new Tile(Color.O, 8));
+		player.addTile(new Tile(Color.R, 5));
+		player.addTile(new Tile(Color.R, 8));
+		ArrayList<Tile> meld = new ArrayList<Tile>();
+		meld.add(new Tile(Color.R, 2));
+		meld.add(new Tile(Color.R, 3));
+		meld.add(new Tile(Color.R, 4));
+		Table.addMeld(meld);
+		player.update(new JRON(new ArrayList<ArrayList<Tile>>(),true,false, new Deck()));
+		player.setFirst30(true);
+		player.playTurn2(Table.getMelds());
+		
+		assertTrue(player.getHand().isEmpty());
+	}
 }
