f188fde7ea4524d9d7ab7d13262a3838473bbdcf
Dallaire <jacobdallaire@yahoo.ca>
2018-11-02 15:00:22 -0400

    added third strategy

--- src/main/java/Rummy/Rummy/ThirdStrategy.java
+++ src/main/java/Rummy/Rummy/ThirdStrategy.java
@@ -16,15 +16,88 @@ public class ThirdStrategy extends Player implements Strategy{
 	 * Play all tiles if possible
 	 * If no player has 3 fewer tiles, then play only tiles that require table input
 	 * */
-	public ArrayList<Tile> playTurn() {
+	public ArrayList<Tile> playTurn() { //legacy method
 		ArrayList<Tile> meldToPlay; 
 		
 		//Default draw from stock
 		this.addTile(Table.getTile());
 		meldToPlay = null;
-		return meldToPlay;
-
-		
-		
+		return meldToPlay;	
+	}
+	
+	public void playTurn2(ArrayList<ArrayList<Tile>> tableMelds) {
+		this.setHasPlayed(false);
+		if (playedFirst30) { //already played once
+			ArrayList<ArrayList<Tile>> temp = new ArrayList<ArrayList<Tile>>();
+			ArrayList<Tile> meld = this.createRun();
+			if (meld == null) {
+				meld = this.createSet();
+			}
+			while (meld != null) {
+				temp.add(meld); //add in the meld
+				meld = null;
+				meld = this.createRun(); //create a new one
+				if (meld == null) {
+					meld = this.createSet();
+				}
+			}
+			ArrayList<Tile> played = new ArrayList<Tile>();
+			for (Tile tile : this.getHand()) { //play all tiles needing stuff on the board
+				for (ArrayList<Tile> tableMeld: Table.getMelds()) {
+					tableMeld.add(tile);
+					if (!MeldChecker.checkHand(tableMeld)) {//invalid play
+						tableMeld.remove(tile); //don't actually play it
+					}
+					else { //valid keep move
+						played.add(tile); //play tile
+						this.setHasPlayed(true);
+						break;
+					}
+				}
+			}
+			this.getHand().removeAll(played); //remove all played tiles
+			
+			if(this.getHand().isEmpty()) { //Win condition
+				Table.getMelds().addAll(temp);
+				this.setHasPlayed(true);
+			}
+			else {
+				if (this.getJRON().getThreeLess()) { //play all possible melds
+					Table.getMelds().addAll(temp);
+					this.setHasPlayed(true);
+				}
+				else{//return functional melds to hand
+					for (ArrayList<Tile> tempMeld : temp) {
+					this.getHand().addAll(tempMeld);
+					}
+					this.getHand().sort(new valueComparator());
+				}
+			}
+		}
+		else { //has yet to play first 30 pts
+			ArrayList<Tile> meld = this.createRun();
+			if(meld == null || !MeldChecker.check30(meld)) {
+				if (meld != null) {
+				this.getHand().addAll(meld);
+				meld.clear();
+				}
+				meld = this.createSet();
+				if(meld != null && MeldChecker.check30(meld)) {
+					Table.addMeld(meld); // plays set as first 30
+					this.setHasPlayed(true);
+					
+				}
+				else { //cant play 30 pts
+					this.getHand().addAll(meld);
+					meld.clear();
+					// Default draw from stock
+					this.addTile(Table.getTile());
+				}
+			}
+			else {
+				Table.addMeld(meld); //plays run as first 30
+				this.setHasPlayed(true);
+			}
+		}
 	}
 }
---
+++ src/test/java/Rummy/Rummy/ThirdStratTests.java
@@ -0,0 +1,7 @@
+package Rummy.Rummy;
+
+import junit.framework.TestCase;
+
+public class ThirdStratTests extends TestCase {
+
+}
