21ed2b11effa6c237f9487f4fe3bdb320db49ab8
Matthew Talbot <matthewtalbot@Matthews-MacBook-Pro.local>
2018-11-03 12:32:52 -0400

    Completely re-did the firststrategy class

--- src/main/java/Rummy/Rummy/FirstStrategy.java
+++ src/main/java/Rummy/Rummy/FirstStrategy.java
@@ -7,51 +7,53 @@ public class FirstStrategy extends Player implements Strategy{
 	public FirstStrategy(String name) {
 		super(name);
 	}
-
-	@Override
-	public ArrayList<ArrayList<Tile>> playTurn() {
-		int sumRun=0;
-		int sumSet=0;
-		// see if we can create a set
-		
-		// see if we can create a run
-		if(createSet()!=null) {
-		 sumSet=MeldChecker.checkSum(createSet());
-		}
-		if (createRun()!=null) {
-			sumRun=MeldChecker.checkSum(createRun());
-
-		}
-		/** check if first 30 pts have been played
-		 * if yes returns a set or a run every time this function is called*/
-		 if (playedFirst30) {
-			return playMeld();
-		}else if (sumRun+sumSet<30) {
-			/** check if you have 30 to play
-			 * if yes returns the melds that will*/
-			this.addTile(Table.getTile());
-			return null;
-		}	
-		else {
-		return playMeld();
-		}
-		}		
+	public ArrayList<ArrayList<Tile>> playTurn(){return null;}
+	public void playTurn(ArrayList<ArrayList<Tile>> tableMelds) { //don't want to return anything, can just interact with table in the function
+		this.setHasPlayed(false);
+		if (tableData.getFirstMeld()) {
+			if (this.playedFirst30) { //First 30 already played
+				ArrayList<ArrayList<Tile>> temp = new ArrayList<ArrayList<Tile>>();
+				ArrayList<Tile> meld = this.createRun();
+				if (meld == null) {
+					meld = this.createSet();
+				}
+				while (meld != null) {
+					temp.add(meld); //add in the meld
+					meld = null;
+					meld = this.createRun(); //create a new one
+					if (meld == null) {
+						meld = this.createSet();
+					}
+				}
+				ArrayList<Tile> played = new ArrayList<Tile>();
+				for (Tile tile : this.getHand()) { //play all tiles needing stuff on the board
+					for (ArrayList<Tile> tableMeld: Table.getMelds()) {
+						tableMeld.add(tile);
+						if (!MeldChecker.checkHand(tableMeld)) {//invalid play
+							tableMeld.remove(tile); //don't actually play it
+						}
+						else { //valid keep move
+							played.add(tile); //play tile
+							this.setHasPlayed(true);
+							break;
+						}
+					}
+				}
+				this.getHand().removeAll(played); //remove all played tiles
+				
+				if(this.getHand().isEmpty()) { //Win condition
+					Table.getMelds().addAll(temp);
+					this.setHasPlayed(true);
+				}
+				else {//return functional melds to hand
+					for (ArrayList<Tile> tempMeld : temp) {
+						this.getHand().addAll(tempMeld);
+					}
+					this.getHand().sort(new valueComparator());
+				}
+			}
 	
-	public ArrayList<Tile> playMeld() {
-		ArrayList<Tile> meldToPlay = new ArrayList<Tile>();
-
-		meldToPlay =createRun();
-		if(meldToPlay!=null) {
-			getHand().removeAll(meldToPlay);
-			return meldToPlay;
+	
 		}
-		meldToPlay=createSet();
-		if (meldToPlay!=null) {
-			getHand().removeAll(meldToPlay);
-			return meldToPlay;
-		}
-		return null;
 	}
-	
-	
 }
