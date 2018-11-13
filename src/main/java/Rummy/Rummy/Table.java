9f525ac88a6340e6c653104c43825275426faf53
Poerilla <tshepok13@gmail.com>
2018-11-13 14:36:13 -0500

    java.io

--- src/main/java/Rummy/Rummy/Table.java
+++ src/main/java/Rummy/Rummy/Table.java
@@ -1,6 +1,8 @@
 package Rummy.Rummy;
 
 import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
 
 
 /**
@@ -35,6 +37,8 @@ public final class Table {
 		for (Player x: players) {
 			x.printTiles();
 		}
+		
+		
 		
 	}
 	
@@ -521,4 +525,48 @@ public final class Table {
 		
 		return "{ " + str + " }";
 	}
+	
+	 public static void main(String[] args) throws IOException {
+	        
+	        if (args.length != 2) {
+	            System.err.println(
+	                "Usage: java EchoClient <host name> <port number>");
+	            System.exit(1);
+	        }
+
+	        String hostName = args[0];
+	        int portNumber = Integer.parseInt(args[1]);
+
+	        try (
+	            Socket kkSocket = new Socket(hostName, portNumber);
+	            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+	            BufferedReader in = new BufferedReader(
+	                new InputStreamReader(kkSocket.getInputStream()));
+	        ) {
+	            BufferedReader stdIn =
+	                new BufferedReader(new InputStreamReader(System.in));
+	            String fromServer;
+	            String fromUser;
+
+	            while ((fromServer = in.readLine()) != null) {
+	                System.out.println("Server: " + fromServer);
+	                if (fromServer.equals("Bye."))
+	                    break;
+	                
+	                fromUser = stdIn.readLine();
+	                if (fromUser != null) {
+	                    System.out.println("Client: " + fromUser);
+	                    out.println(fromUser);
+	                }
+	            }
+	        } catch (UnknownHostException e) {
+	            System.err.println("Don't know about host " + hostName);
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.err.println("Couldn't get I/O for the connection to " +
+	                hostName);
+	            System.exit(1);
+	        }
+	    }
+	
 }
