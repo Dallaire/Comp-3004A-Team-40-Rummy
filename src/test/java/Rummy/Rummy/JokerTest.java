72381b8434fed477ad7077a74e35c4096cc15f80
Dallaire <jacobdallaire@yahoo.ca>
2018-11-27 10:42:33 -0500

    added test for joker in run

--- src/test/java/Rummy/Rummy/JokerTest.java
+++ src/test/java/Rummy/Rummy/JokerTest.java
@@ -13,10 +13,23 @@ public class JokerTest extends TestCase {
 		player.addTile(new Tile(Color.B, 8));
 		player.addTile(new Tile(Color.O, 8));
 		player.addTile(new Tile(Color.R, 8));
-		player.addTile(new Joker(null,0));
+		player.addTile(new Tile(null,0));
 		player.setFirst30(true);
 		player.playTurn();
-		System.out.println(Table.getMelds());
+		assertTrue(player.getHand().isEmpty());
+	}
+	
+	public void testJokerRun() {
+		Table.getMelds().clear();
+		FirstStrategy player = new FirstStrategy("p1", true);
+		player.getHand().clear();
+		player.addTile(new Tile(Color.B, 8));
+		player.addTile(new Tile(Color.B, 9));
+		player.addTile(new Tile(Color.B, 7));
+		player.addTile(new Tile(null,0));
+		player.setFirst30(true);
+		player.playTurn();
+		System.out.println(player.getHand().size());
 		assertTrue(player.getHand().isEmpty());
 	}
 }
