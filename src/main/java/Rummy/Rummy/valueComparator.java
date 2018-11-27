b91f0e30a7dac870aa759020f8ceb35eba6cdb22
Dallaire <jacobdallaire@yahoo.ca>
2018-11-27 10:24:18 -0500

    adjustments to handle joker

--- src/main/java/Rummy/Rummy/valueComparator.java
+++ src/main/java/Rummy/Rummy/valueComparator.java
@@ -10,7 +10,12 @@ public class valueComparator implements Comparator<Tile> {
 	    public int compare(Tile o1, Tile o2) {
 		 	Color c1 = o1.getColor();
 		 	Color c2 = o2.getColor();
-		 	int comp = c1.compareTo(c2);
+		 	int comp;
+		 	if (c1 == null) 
+		 		comp = -1;
+		 	else if (c2 == null)
+		 		comp = 1;
+		 	else comp = c1.compareTo(c2);
 		 	
 		 	if (comp != 0) {return comp;}
 		 	
