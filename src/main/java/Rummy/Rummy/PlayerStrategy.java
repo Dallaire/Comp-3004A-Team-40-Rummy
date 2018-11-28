083a3dd7ff2348e68c35af3bb6485a360791be12
Matthew Talbot <matthewtalbot@dhcp-148-250.cu-wireless-students.carleton.ca>
2018-11-28 16:56:06 -0500

    Added a timer for the player

--- src/main/java/Rummy/Rummy/PlayerStrategy.java
+++ src/main/java/Rummy/Rummy/PlayerStrategy.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Timer;
+
 
 public class PlayerStrategy extends Player implements Strategy {
 	
@@ -27,8 +27,10 @@ public class PlayerStrategy extends Player implements Strategy {
 	 * -Two modes: can read from file or prompt user through the console
 	 * @return - ArrayList<ArrayList<Tile>> if valid  or null if passing and or a meld*/
 	public ArrayList<ArrayList<Tile>> playTurn() {
+
 		
 		// local variables
+
 		int points = 0;
 		boolean flag = true;		
 		ArrayList<ArrayList<Tile>> melds = new ArrayList<ArrayList<Tile>>();
